c313ae220e6c83e941d20cc93ead0feaa6658804
ashim <ashimgautam01@gmail.com>
2024-08-28 20:13:27 +0545

    updated

--- src/Schemas/LoginSchema.ts
+++ src/Schemas/LoginSchema.ts
@@ -6,7 +6,5 @@ export const LogInSchema = z.object({
   password: z
     .string()
     .min(5, { message: "password Should be atleast 5 character" })
-    // .regex(
-    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    // ),
+  
 });
--- src/Schemas/SignInSchema.ts
+++ src/Schemas/SignInSchema.ts
@@ -11,8 +11,6 @@ export const SignInSchema = z.object({
   password: z
     .string()
     .min(5, { message: "password Should be atleast 5 character" }),
-    // .regex(
-    //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    // ),
+   
    
 });
