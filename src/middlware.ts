14d46ebddd9cd03af045440277c15c58ed8026a4
ashim <ashimgautam01@gmail.com>
2024-08-28 20:16:56 +0545

    updated

--- src/middlware.ts
+++ src/middlware.ts
@@ -24,5 +24,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/sign-in", "/sign-up", "/verify", "/dashboard"],
+  matcher: ["/sign-in", "/sign-up", "/verify", "/"],
 };
